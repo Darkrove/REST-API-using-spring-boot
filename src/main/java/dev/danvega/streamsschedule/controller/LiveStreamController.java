8dc0a336629e888038bdd94c9e86c09532ec2828
Sajjad Shaikh <samaralishaikh212@gmail.com>
2022-07-28 10:51:20 +0530

    Added 'CRUD' Operation

--- src/main/java/dev/danvega/streamsschedule/controller/LiveStreamController.java
+++ src/main/java/dev/danvega/streamsschedule/controller/LiveStreamController.java
@@ -19,21 +19,26 @@ public class LiveStreamController {
         this.repository = repository;
     }
 
-
+    // GET http://localhost:8080/streams/
     @GetMapping
     public List<LiveStream> findAll() {
         return repository.findAll();
     }
 
+    // GET http://localhost:8080/streams/id={id}
     @GetMapping("/id={id}")
     public LiveStream findById(@PathVariable String id) throws LiveStreamNotFoundException {
         return repository.findById(id);
     }
+
+    // DELETE http://localhost:8080/streams/id={id}
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/id={id}")
     public void delete(@PathVariable String id) throws LiveStreamNotFoundException {
         repository.delete(id);
     }
+
+    // POST http://localhost:8080/streams/
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public LiveStream create(@RequestBody LiveStream stream) {
